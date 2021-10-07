ce384af311d8e7156151c0ec886f236e3960ef7e
diningX <hirosuke1309@gmail.com>
2021-10-08 08:52:53 +0900

    commitment

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-# youtube-highlight
-youtube自動切り抜き、ハイライト生成、自動投稿
+# comment-app
+youtubeライブからコメントを取得
 
 
 環境設定
 
-・pythonとrubyの環境を構築する
-・ruby 2.7.1
-・Python 3.9.1
-
-gem install video_chat_get
+`Python 3.9.1`
 
 
 使い方
 
-streamlit run highlight.py
+`streamlit run src/codes/main.py`
 
 で実行
+
+# If you run it locally
+
+Run `run.py` in the `continuation_livechat_crawler` with `URL`.
+
+key：firebeseの秘密鍵のjsonファイル
+bucket_name： storageの名前
 
---
+++ src/secret/secret.py
@@ -0,0 +1,14 @@
+key={
+  "type": "service_account",
+  "project_id": "chat-crawl-708ee",
+  "private_key_id": "739530d01adba460f1ffedfc64b1bbac8711cc4a",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCp3vaCgfzgB3pn\nAGJBV6ilFNHmNYqkW3gv8UG8NJAhR4m0SjSygo+eOmw79CMhXzsFrdiDipBgJqXW\nwkMZ1VA5ORmVQXwOr+LbKPqxoX1v0qyiBFxUOlSMC/7RBQHL9L2oJGh5SoOnsnIT\nxPfPf6Xk2lgbsgbIyAxJIXHBoo2OJ4d4bAJnT6Vp4xU1dn/VVx/nlpAxXBJ1cRCg\nvErcxilFOqfA6cVCBFi+M+SmYtBzBRuMa+5JaG7Rj96RLp6+1r460hHUWMHcNxm4\nWCZvAfhysimMHKen/MwPFYbZZ15OOvsjyFv3YxlTzIC/iirgoVcEVCYeMXb2uE6B\nDqDv416nAgMBAAECggEABmJuExdN86Du/Z+65v148K9uI4AKqcUS/iuhTrC5DrnQ\nwqPGru46S0ujKsOxGFLVyzpZTg8EqyB4u4V0noFxG516SSz7Zn/qKlY0Hz3/EsIG\ndqvBnpwh9luIZGm9mLamSYNeIS+Rmtlu8TBGJ23YRILdAeQjM3kAATWjwA/bJdG7\nKhjV/6bpSYy6WenDI21/oBRo+Tl8GJQyqk+HGHU5BLrqNtv2bQktV8LwFlSp72eW\nmLF3m4+s9CBOjI4E9xtrLgZEj9XNp4/pMOO3BsCo55/4eOlN5OjYq3tB1800wX0f\nuUSSWCJuTJp7mEVG9CRSPiEAqOhnI/8Or3GFL2sSrQKBgQDZdHeL6xoXjiLqIYbD\nsol60UaiRmENCwYQj3mQrVBUyuPqidb/URHtg2GO3k9fOkxcbtcQhIteC2RzG9fB\nmIOFAAaoZmAqQYTsJ/rUAYv+UAYvl3hiZHh45i2T9WLJyHh32M2M5I+zduMMawux\nlDtfR29zTFdTivlln8pOjf+3TQKBgQDH+0JiTnREAIGPW1wui2VYJw8NrBbS98BR\n3EUyXbYUS+xRvTIPv/egxy+uWuMsV7my+nI1ZbysDPjAnBk7BWiEV926uk4lYWfg\nHwmLeM39kfpQKVuo/4nF4hWgo+v+lNekCuAare6QxAIm8bh/AXhOZ4XiGbbL9LTg\n9rmCHEw7wwKBgQCmDhhUUp5EKphi7mM5AheWIEWuGcx251okP0v3vUHA3oaZTdVO\ndxwXoeJX9U/rSluRolq9FFpQ4KY+eJ6UCeE1KUXsRvFck1fr9sPGLcA33eg7Y5Hc\noRiWSj+34wUjTeEgtACTfN53j2vmyb/ORYHpSmXDSCLPeyrj/dfBkRIbaQKBgCuc\na7msikZaJebxcYTk+VDkE7BTmxae2N4BP0XgkdiTRhH9RHGS15nEVm2bfHcrGmAh\npc5nRWHv4j9+yzHH2CckUkJqZqhYOaLyoc48pg9qSkA5BLgyIgV5bGX/2XNHWk93\n369xxtjjJeUASc/IrkfK47OPEAIid1+n7oSlTyqpAoGBAKzyIIkECp0ohhqEXl1v\n0neCyE7SttY+BpvEveONSfkVkLz0jCp87DQHx+YyFmPgvawAxJvWFMnJHYtkhe1q\nbdqmH62uvJNqiRnlBrWKcXqwdU1kNutm8dFNxijmb2pAQI/2s7r9hiFtthNoxYta\nS/QJ/O2K6TUXzCjaZ561nTWi\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-ptw8q@chat-crawl-708ee.iam.gserviceaccount.com",
+  "client_id": "110918171689088650018",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ptw8q%40chat-crawl-708ee.iam.gserviceaccount.com"
+}
+
+bucket_name='chat-crawl-708ee.appspot.com'
